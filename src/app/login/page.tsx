ba299fd028bd7bfcd1ccbb5ed3318b2bf0a25ebd
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2026-04-05 22:08:20 +0900

    feat: ログインページを作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
           <button
             onClick={handleGoogleLogin}
             type="button"
-            className="group relative flex w-full justify-center items-center gap-3 rounded-xl bg-white dark:bg-zinc-700 py-3.5 px-4 text-sm font-bold text-zinc-700 dark:text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-300 dark:ring-zinc-500 hover:bg-zinc-50 dark:hover:bg-zinc-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all hover:shadow-md active:scale-[0.98]"
+            className="group relative flex w-full justify-center items-center gap-3 rounded-xl bg-white dark:bg-zinc-700 py-3.5 px-4 text-sm font-bold text-zinc-700 dark:text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-300 dark:ring-zinc-500 hover:bg-zinc-50 dark:hover:bg-zinc-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all hover:shadow-md active:scale-[0.98] hover:cursor-pointer"
           >
             <svg
               className="h-5 w-5 pointer-events-none"
