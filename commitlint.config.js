261344fbfc0c8f6842644be17d09360cdc8cfbcb
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2026-04-05 11:20:19 +0900

    docs: commitlintで使用できるvalueをコメントアウトに追加

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,16 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default { extends: ["@commitlint/config-conventional"] };
+
+// values
+// [
+//   "build", ビルドシステムまたは外部依存関係の変更
+//   "chore", ビルドプロセスまたは補助ツールとライブラリの変更
+//   "ci", CI 設定とスクリプトの変更
+//   "docs", ドキュメントのみの変更
+//   "feat", 新機能の追加
+//   "fix", バグ修正
+//   "perf", パフォーマンス改善
+//   "refactor", リファクタリング
+//   "revert", コミットの取り消し
+//   "style", コードスタイル
+//   "test", テスト
+// ];
