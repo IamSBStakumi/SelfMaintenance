2d482052acbd757e120eb5c62033d48a9272b0bf
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2026-04-15 20:57:31 +0900

    config: global.cssで定義したカラーテーマを削除

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,6 @@ module.exports = {
         "3xl": "1.5rem",
         "4xl": "2rem",
       },
-      colors: {
-        "soft-pink": "#FFB7C5",
-        mint: "#98FB98",
-        lavender: "#E6E6FA",
-      },
       boxShadow: {
         soft: "0 10px 25px -5px rgba(0, 0, 0, 0.05)",
       },
