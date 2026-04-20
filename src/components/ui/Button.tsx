2cb5ec279f1152c7cc59370312117cde825139a2
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2026-04-21 07:50:36 +0900

    refactor: ButtonのPropsの型定義を汎用化

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,14 @@
-type Props = {
-  onClick: () => void;
+type Props = React.ComponentProps<"button"> & {
   children: React.ReactNode;
 };
 
-const Button = ({ onClick, children }: Props) => {
+const Button = ({ onClick, children, ...props }: Props) => {
   return (
     <button
       onClick={onClick}
       type="button"
       className="group relative flex w-full justify-center items-center gap-3 rounded-xl bg-white dark:bg-zinc-700 py-3.5 px-4 text-sm font-bold text-zinc-700 dark:text-zinc-100 shadow-sm ring-1 ring-inset ring-zinc-300 dark:ring-zinc-500 hover:bg-zinc-50 dark:hover:bg-zinc-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all hover:shadow-md active:scale-[0.98] hover:cursor-pointer"
+      {...props}
     >
       {children}
     </button>
