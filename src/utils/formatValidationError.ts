f0e0d87ddf007481ba574b1a225ae090d4223b30
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2026-05-27 09:19:05 +0900

    refactor: エラーの返り値をシンプルに

--- src/utils/formatValidationError.ts
+++ src/utils/formatValidationError.ts
@@ -1,24 +1,7 @@
 import { ZodError } from "zod";
 
 const formatValidationError = (error: ZodError) => {
-  if (
-    error &&
-    typeof error === "object" &&
-    "issues" in error &&
-    Array.isArray(error.issues)
-  ) {
-    const firstIssue = error.issues[0];
-    if (
-      firstIssue &&
-      typeof firstIssue === "object" &&
-      "message" in firstIssue &&
-      typeof firstIssue.message === "string"
-    ) {
-      return firstIssue.message;
-    }
-  }
-
-  return "入力内容を確認してください。";
+  return error.issues?.[0]?.message || "入力内容を確認してください。";
 };
 
 export default formatValidationError;
