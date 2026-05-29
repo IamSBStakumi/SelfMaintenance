09969a64bbf8299f0b144eb92cbaa5479dcaa526
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2026-05-29 09:17:08 +0900

    fix: childrenの書き方をほかの箇所と統一

--- src/components/TaskFormWrapper.tsx
+++ src/components/TaskFormWrapper.tsx
@@ -1,6 +1,4 @@
-const TaskFormWrapper: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+const TaskFormWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen bg-zinc-50 px-4 py-6 dark:bg-zinc-900 font-sans text-zinc-900 dark:text-zinc-100 flex flex-col items-center sm:p-6">
       <div className="max-w-2xl w-full">{children}</div>
